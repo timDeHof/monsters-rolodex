020bef9ea6e93d87a851d4d2bad065cf38e357a0
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-05 20:14:21 -0400

    added a componentDidMount() to app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component }  from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,18 +7,30 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Tim'
+      monsters : [],
     }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json()).then(
+        (users) => this.setState(()=> {
+          return {monsters: users}
+        },
+        () => console.log(this.state)
+        ))
   }
 
   render(){
    return (
      <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>Hi {this.state.name}</p>
-         <button>Change Name</button>
-       </header>
+       {
+        this.state.monsters.map((monster)=> {
+          return (<div key={monster.id}><h1>{monster.name}
+          </h1>
+          </div>)
+        })
+       }
      </div>
    );
   }   
