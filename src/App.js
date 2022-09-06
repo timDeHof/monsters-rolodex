d36aea661020e963a55a4b6a3160611af043ad93
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-05 21:38:29 -0400

    created a search input and optimizated the code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
-import { Component }  from 'react';
+import { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters : [],
-    }
+      monsters: [],
+      searchField: ""
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json()).then(
-        (users) => this.setState(()=> {
-          return {monsters: users}
-        },
-        () => console.log(this.state)
-        ))
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+  render() {
+    const { monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
 
-  render(){
-   return (
-     <div className="App">
-       {
-        this.state.monsters.map((monster)=> {
-          return (<div key={monster.id}><h1>{monster.name}
-          </h1>
-          </div>)
-        })
-       }
-     </div>
-   );
-  }   
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
