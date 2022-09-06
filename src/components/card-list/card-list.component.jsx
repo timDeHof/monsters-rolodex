28c7866ae3d85ac662a7e557c00b0082bddecc9a
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-06 19:31:13 -0400

    created a card-list component and styled it

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,14 @@
 import { Component } from "react";
 
+import "./card-list.styles.css";
+import Card from "../card/card.component";
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
