f10123a8697ff56d3e4c6c372e5702e32cd8c4eb
timDeHof <this.is.tim.dehof@gmail.com>
2022-10-19 22:31:56 -0400

    updated the card info and styled them to flip on hover

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,15 +1,64 @@
 import "./card.styles.css";
-
+import { MdEmail } from "react-icons/md";
+import {
+  FaHome,
+  FaTwitter,
+  FaInstagram,
+  FaFacebook,
+  FaDribbble,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { BsGlobe, BsFillTelephoneFill } from "react-icons/bs";
 const Card = ({ monster }) => {
-  const { name, email, id } = monster;
+  const { name, email, id, website, phone, address } = monster;
+  const { city, street, suite, zipcode } = address;
+
   return (
-    <div className="card-container" key={id}>
-      <img
-        alt={`monster ${name}`}
-        src={`https://robohash.org/${id}?set=set2&size=180x180`}
-      />
-      <h2>{name}</h2>
-      <p>{email}</p>
+    <div className='card-container' key={id}>
+      <div className='single-card'>
+        <div className='front'>
+          <img
+            alt={`monster ${name}`}
+            src={`https://robohash.org/${id}?set=set2&size=180x180`}
+          />
+          <div className='content'>
+            <h2>{name}</h2>
+          </div>
+        </div>
+
+        <div className='back'>
+          <div className='content'>
+            <div className='address'>
+              <FaHome />
+              <p>
+                {street} {suite}
+              </p>
+              <p>
+                {city} {zipcode}
+              </p>
+            </div>
+            <div className='email'>
+              <MdEmail />
+              <p>{email}</p>
+            </div>
+            <div className='website'>
+              <BsGlobe />
+              <p>{website}</p>
+            </div>
+            <div className='phone'>
+              <BsFillTelephoneFill />
+              <p>{phone}</p>
+            </div>
+            <div className='socials'>
+              <FaTwitter />
+              <FaInstagram />
+              <FaFacebook />
+              <FaDribbble />
+              <FaLinkedinIn />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
